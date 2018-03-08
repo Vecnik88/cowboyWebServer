c1e67246cf834c7ede21a8647d877b6ff83d0d4a
Vecnik <Ve_cni_k@inbox.ru>
2018-03-08 09:11:06 +0700

    web server push

---
+++ _rel/hello_erlang_release/lib/hello_erlang-0.1.0/src/hello_handler.erl
@@ -0,0 +1,19 @@
+-module(hello_handler).
+-behavior(cowboy_handler).
+
+-export([init/2]).
+
+%init(Req0, State) ->
+ %   Req = cowboy_req:reply(200,
+  %      #{<<"content-type">> => <<"text/plain">>},
+   %     <<"Hello Erlang!">>,
+    %    Req0),
+    %io:format("~p~n", [Req0]),
+   % {ok, Req, State}.
+init(Req0, State) ->
+    Req = cowboy_req:reply(200,
+        #{<<"content-type">> => <<"text/plain">>},
+        <<"Hello Erlang!">>,
+        Req0),
+        io:format("~p~n", [Req0]),
+    {ok, Req, State}.
